a5a9d826f5075494866f1a612e2987f98c94d4ad
Johnathan <jriggins@unomaha.edu>
2024-05-10 16:02:02 -0500

    Removed attempt checker in grader

--- grader.py
+++ grader.py
@@ -8,27 +8,28 @@ class Grader:
         """
         self.__name: str = ''
         self.__attempts: int = 0
-        self.__scores: List[int] = [0, 0, 0, 0]
+        # self.__scores: List[int] = [0, 0, 0, 0]
+        self.__scores: List[int] = []
 
     @staticmethod
     def check_number(num: int) -> None:
         if num < 0:
             raise ValueError('Negative')
 
-    def check_attempt_count(self, amount: int) -> None:
-        self.check_number(amount)
-        if amount < 1 or amount > 4:
-            raise ValueError('Out of attempt range')
-
-    def set_attempt_count(self, amount_str: str) -> None:
-        # print(type(amount_str))
-        amount: int = int(amount_str)
-        self.check_attempt_count(amount)
-        self.__attempts = amount
-        # print(self.__attempts)
-
-    def get_attempt_count(self) -> int:
-        return self.__attempts
+    # def check_attempt_count(self, amount: int) -> None:
+    #     self.check_number(amount)
+    #     if amount < 1 or amount > 4:
+    #         raise ValueError('Out of attempt range')
+    #
+    # def set_attempt_count(self, amount_str: str) -> None:
+    #     # print(type(amount_str))
+    #     amount: int = int(amount_str)
+    #     self.check_attempt_count(amount)
+    #     self.__attempts = amount
+    #     # print(self.__attempts)
+    #
+    # def get_attempt_count(self) -> int:
+    #     return self.__attempts
 
     def check_score(self, score: int) -> None:
         self.check_number(score)
